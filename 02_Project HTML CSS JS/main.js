8aab722eccb0b068b52931b7fd284c0be303cdf6
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-06-07 22:16:49 +0300

    Update main.js

--- 02_Project HTML CSS JS/main.js
+++ 02_Project HTML CSS JS/main.js
@@ -1,8 +1,24 @@
-const buttons = document.querySelectorAll(".btn");
-buttons.forEach(but => {
-    but.addEventListener("click", function() {
-    const content = but.closest(".accordion-header").nextElementSibling;
-     content.classList.toggle("active");
-     but.textContent = content.classList.contains("active") ? "-" : "+";
+// const buttons = document.querySelectorAll(".btn");
+// buttons.forEach(but => {
+//     but.addEventListener("click", function() {
+//     const content = but.closest(".accordion-header").nextElementSibling;
+//      content.classList.toggle("active");
+//      but.textContent = content.classList.contains("active") ? "-" : "+";
+//     })
+// })
+
+const item = document.querySelectorAll(".accordion-item");
+
+item.forEach(ele => {
+    const header = ele.querySelector(".accordion-header");
+    const content = ele.querySelector(".accordion-content");
+    const butt = ele.querySelector(".btn");
+    
+    header.addEventListener("click", function(){
+        content.classList.toggle("active");
+        if (content.classList.contains("active")){
+            butt.textContent = "-";
+        }
+        else butt.textContent = "+";
     })
 })
